68ca2db6dbace11f7a4f4c9d84dbebe4d44dae29
PresencePro <160008815+PresencePro@users.noreply.github.com>
2024-03-03 23:52:36 +0530

    udated toolbar title

--- app/src/main/java/administratorpkg/administrotor_panel.java
+++ app/src/main/java/administratorpkg/administrotor_panel.java
@@ -1,5 +1,6 @@
 package administratorpkg;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -21,7 +22,7 @@ public class administrotor_panel extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    ImageButton notification;
+//    ImageButton notification;
 
       ViewPager vpager;
       TabLayout tabLayout;
@@ -30,12 +31,14 @@ public class administrotor_panel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrotor_panel);
-        notification=findViewById(R.id.notificationbtn);
+//        notification=findViewById(R.id.notificationbtn);
         drawerLayout=findViewById(R.id.Drawer_lay);
         navigationView=findViewById(R.id.nav_view);
         toolbar=findViewById(R.id.toolbar);
-
+      
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(" ");
+
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
@@ -47,12 +50,12 @@ public class administrotor_panel extends AppCompatActivity {
         vpager.setAdapter(myFragmentAdapter);
         tabLayout=findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(vpager);
-        notification.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(administrotor_panel.this, "clicked notification icon", Toast.LENGTH_LONG).show();
-            }
-        });
+//        notification.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(administrotor_panel.this, "clicked notification icon", Toast.LENGTH_LONG).show();
+//            }
+//        });
 
     }
 
