b43f0dc6cf36d581c5a54cc2f689f64908912b64
PresencePro <160008815+PresencePro@users.noreply.github.com>
2024-03-03 18:31:38 +0530

    pragati1 menubar

--- app/src/main/java/administratorpkg/administrotor_panel.java
+++ app/src/main/java/administratorpkg/administrotor_panel.java
@@ -1,6 +1,9 @@
 package administratorpkg;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
@@ -11,10 +14,13 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.loginform.R;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class administrotor_panel extends AppCompatActivity {
-
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     ImageButton notification;
 
       ViewPager vpager;
@@ -25,6 +31,16 @@ public class administrotor_panel extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrotor_panel);
         notification=findViewById(R.id.notificationbtn);
+        drawerLayout=findViewById(R.id.Drawer_lay);
+        navigationView=findViewById(R.id.nav_view);
+        toolbar=findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         vpager=findViewById(R.id.vpager);
         myFragmentAdapter=new FragmentAdapter(getSupportFragmentManager());
