112f9386e4ab61d086369e7f635677a2ebceee40
PresencePro <160008815+PresencePro@users.noreply.github.com>
2024-03-03 17:44:53 +0530

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 188aed7a5b4de8b1c533dcfb54c718b2dc6db3cd, reversing
    changes made to 727b0615440572ca46c93da73fc47ff09ae69bb0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,14 @@ android {
 }
 
 dependencies {
+    constraints {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0") {
+            because("kotlin-stdlib-jdk7 is now a part of kotlin-stdlib")
+        }
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0") {
+            because("kotlin-stdlib-jdk8 is now a part of kotlin-stdlib")
+        }
+    }
     implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.4.0")
@@ -43,11 +51,17 @@ dependencies {
     implementation("androidx.annotation:annotation:1.7.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.navigation:navigation-fragment:2.6.0")
+    implementation("androidx.navigation:navigation-ui:2.6.0")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
+    implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     configurations.implementation{
         exclude("org.jetbrains.kotlin",("kotlin-stdlib-jdk8"))
+
     }
 
 }
