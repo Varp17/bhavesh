543dcb42b9f2789bdd86e4f2b5c5c48ca651c0de
Varun Patil <varunpatil0217@gmail.com>
2024-03-04 21:27:19 +0530

    String Complexity

--- app/src/main/java/administratorpkg/Managestaff_activity.java
+++ app/src/main/java/administratorpkg/Managestaff_activity.java
@@ -3,14 +3,13 @@ package administratorpkg;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
@@ -18,7 +17,7 @@ import com.example.loginform.R;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class Managestaff_activity extends AppCompatActivity {
+public class Managestaff_activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
@@ -33,25 +32,31 @@ public class Managestaff_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.managestaff_activity);
 //        notification=findViewById(R.id.notificationbtn);
+        /* ----------------------------Hooks--------------------------*/
         drawerLayout=findViewById(R.id.Drawer_lay);
         navigationView=findViewById(R.id.nav_view);
         toolbar=findViewById(R.id.toolbar);
-
+/* ----------------------------toolbar--------------------------*/
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Manage Staff");
 
+/*----------------------------Navigation Drawer Menu ---------------------------*/
+//       Menu menu = navigationView.getMenu();
+//       menu.findItem(R.id.nav_home).setVisible(false);
+//       menu.findItem(R.id.timetable).setVisible(false);
+//       menu.findItem(R.id.nav_feedback).setVisible(false);
 
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
+        navigationView.setNavigationItemSelectedListener(this);
 
         vpagermanage=findViewById(R.id.vpagermanage);
         managestaffadapter=new managestaffadapter(getSupportFragmentManager());
         vpagermanage.setAdapter(managestaffadapter);
         tabLayout=findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(vpagermanage);
-
     }
 
     @Override
@@ -62,5 +67,10 @@ public class Managestaff_activity extends AppCompatActivity {
 
         return true;
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
