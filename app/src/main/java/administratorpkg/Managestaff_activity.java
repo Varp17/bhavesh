584e3c168e195de6d33eb2b158d3416049d49a62
PresencePro <160008815+PresencePro@users.noreply.github.com>
2024-03-04 12:46:37 +0530

    managestaff files created

--- app/src/main/java/administratorpkg/Managestaff_activity.java
+++ app/src/main/java/administratorpkg/Managestaff_activity.java
@@ -1,41 +1,42 @@
 package administratorpkg;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.viewpager.widget.ViewPager;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.loginform.R;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class administrotor_panel extends AppCompatActivity {
+public class Managestaff_activity extends AppCompatActivity {
+
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
 //    ImageButton notification;
 
-      ViewPager vpager;
-      TabLayout tabLayout;
-      FragmentAdapter myFragmentAdapter;
+    ViewPager vpagermanage;
+    TabLayout tabLayout;
+    managestaffadapter managestaffadapter ;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_administrotor_panel);
+        setContentView(R.layout.managestaff_activity);
 //        notification=findViewById(R.id.notificationbtn);
         drawerLayout=findViewById(R.id.Drawer_lay);
         navigationView=findViewById(R.id.nav_view);
         toolbar=findViewById(R.id.toolbar);
-      
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(" ");
 
@@ -45,11 +46,11 @@ public class administrotor_panel extends AppCompatActivity {
         toggle.syncState();
 
 
-        vpager=findViewById(R.id.vpager);
-        myFragmentAdapter=new FragmentAdapter(getSupportFragmentManager());
-        vpager.setAdapter(myFragmentAdapter);
+        vpagermanage=findViewById(R.id.vpagermanage);
+        managestaffadapter=new managestaffadapter(getSupportFragmentManager());
+        vpagermanage.setAdapter(managestaffadapter);
         tabLayout=findViewById(R.id.tabLayout);
-        tabLayout.setupWithViewPager(vpager);
+        tabLayout.setupWithViewPager(vpagermanage);
 
     }
 
