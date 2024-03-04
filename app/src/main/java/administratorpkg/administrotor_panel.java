98216a00a124ad2bde2e6d9f4d41a59ce8ef3a1e
Himanshu Yeole <yeolevansh123@gmail.com>
2024-03-04 13:49:57 +0530

    prerna3.0

--- app/src/main/java/administratorpkg/administrotor_panel.java
+++ app/src/main/java/administratorpkg/administrotor_panel.java
@@ -1,15 +1,18 @@
 package administratorpkg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -18,7 +21,7 @@ import com.example.loginform.R;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class administrotor_panel extends AppCompatActivity {
+public class administrotor_panel extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -39,10 +42,11 @@ public class administrotor_panel extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("PresencePro");
 
-
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
 
         vpager=findViewById(R.id.vpager);
@@ -67,5 +71,19 @@ public class administrotor_panel extends AppCompatActivity {
 
         return true;
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
