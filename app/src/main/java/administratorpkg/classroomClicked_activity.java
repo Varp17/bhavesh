b003c4ec53442bd87c856911933962e37b3cc637
PresencePro <160008815+PresencePro@users.noreply.github.com>
2024-03-06 12:35:52 +0530

    tp

---
+++ app/src/main/java/administratorpkg/classroomClicked_activity.java
@@ -0,0 +1,86 @@
+package administratorpkg;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.loginform.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import administratorpkg.FragmentAdapter;
+
+public class classroomClicked_activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    Toolbar toolbar;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+//    ImageButton notification;
+
+    ViewPager vpager;
+    TabLayout tabLayout;
+    classroom_activity_adapter myFragmentAdapter;
+    Menu menu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.classroom_clicked_activity);
+
+        drawerLayout=findViewById(R.id.Drawer_lay);
+        navigationView=findViewById(R.id.nav_view);
+        toolbar=findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Subject Name");
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        vpager=findViewById(R.id.vpager);
+        myFragmentAdapter=new classroom_activity_adapter(getSupportFragmentManager());
+        vpager.setAdapter(myFragmentAdapter);
+        tabLayout=findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(vpager);
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menubaradmin, menu);
+
+        return true;
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
+    }
+
+}
