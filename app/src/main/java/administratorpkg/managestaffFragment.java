228a0332e3202045bf1d0d960aed7b35bb0ed6f2
Varun Patil <varunpatil0217@gmail.com>
2024-03-09 16:03:42 +0530

    Chotu Chohan

--- app/src/main/java/administratorpkg/managestaffFragment.java
+++ app/src/main/java/administratorpkg/managestaffFragment.java
@@ -1,14 +1,22 @@
 package administratorpkg;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.loginform.R;
+
+import java.sql.ClientInfoStatus;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +33,7 @@ public class managestaffFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     public String mParam2;
+    private ListView listView;
 
     public managestaffFragment() {
         // Required empty public constructor
@@ -69,5 +78,13 @@ public class managestaffFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_managestaff, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
