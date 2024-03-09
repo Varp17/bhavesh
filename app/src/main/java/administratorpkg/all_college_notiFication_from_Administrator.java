2648d29d4edf4814c7f348e4abffeed478d0f8ad
Varun Patil <varunpatil0217@gmail.com>
2024-03-09 18:06:25 +0530

    Notification Button

--- app/src/main/java/administratorpkg/all_college_notiFication_from_Administrator.java
+++ app/src/main/java/administratorpkg/all_college_notiFication_from_Administrator.java
@@ -1,16 +1,20 @@
 package administratorpkg;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.loginform.R;
 
 public class all_college_notiFication_from_Administrator extends varchi_line {
+
+    private Dialog myDialog;
+    Button notifycollegebtn;
 
     @Override
     int getLayoutresId() {
@@ -26,5 +30,29 @@ public class all_college_notiFication_from_Administrator extends varchi_line {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Assuming you have a Button with ID notifycollegebtn in your layout file
+        notifycollegebtn = findViewById(R.id.notifycollegebtn);
+        notifycollegebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotificationDialog();
+            }
+        });
+    }
+
+    private void showNotificationDialog() {
+        // Assuming you have a layout file named administrator_notification_edit_panel
+        myDialog = new Dialog(all_college_notiFication_from_Administrator.this);
+        myDialog.setContentView(R.layout.administrator_notification_edit_panel);
+
+        TextView notification_close_btn = myDialog.findViewById(R.id.notification_close_btn);
+        notification_close_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+
+        myDialog.show();
     }
 }
