98770768dc30e60ce2b4da1d238fd289d090c726
Varun Patil <varunpatil0217@gmail.com>
2024-03-27 21:33:22 +0530

    Teacher Interface Correction

---
+++ app/src/main/java/teacherpkg/notification_teacher.java
@@ -0,0 +1,20 @@
+package teacherpkg;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.loginform.R;
+
+public class notification_teacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_notification_teacher);
+    }
+}
