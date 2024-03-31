c7953b1c4f0e70094a6fc6046180e8fcb7c4e41a
Mrugesh <patilmrugesh84@gmail.com>
2024-03-31 23:18:40 +0530

    Teacher Profile Panel added

--- app/src/main/java/teacherpkg/Teacher_Profile.java
+++ app/src/main/java/teacherpkg/Teacher_Profile.java
@@ -2,58 +2,25 @@ package teacherpkg;
 
 import static android.content.ContentValues.TAG;
 
-import android.app.FragmentTransaction;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.Intent;
-import android.credentials.CreateCredentialRequest;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
+
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import com.example.loginform.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class Teacher_Profile extends varchi_line {
 
-    EditText profile_name,profile_id,profile_branch,profile_dob,profile_address,profile_mobno;
     FirebaseAuth mAuth;
     FirebaseFirestore fstore;
 
@@ -61,9 +28,6 @@ public class Teacher_Profile extends varchi_line {
 
 
     int getProfile_values(){
-
-
-
         return 0;
     }
     @Override
