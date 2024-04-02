9fd2b0921d67c3da2a40362664cd8736bc5cc92c
Mrugesh <patilmrugesh84@gmail.com>
2024-04-02 20:59:07 +0530

    Login Page updated.......Manage Student in teacher panel created successfullyy

--- app/src/main/java/administratorpkg/managestaffFragment.java
+++ app/src/main/java/administratorpkg/managestaffFragment.java
@@ -62,7 +62,7 @@ import java.util.Map;
  */
 
 
-public class managestaffFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
+public class managestaffFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -71,9 +71,9 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private ArrayList<Staff>staffArrayList;
+    private ArrayList<Staff> staffArrayList;
     private ArrayList<String> teachername;
-    private ArrayList<String> teacherId,password,staffemail;
+    private ArrayList<String> teacherId, password, staffemail;
     private RecyclerView recyclerview;
     private FirebaseAuth mAuth;
     private FirebaseFirestore fstore;
@@ -118,11 +118,9 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
 
 
 //        View view = inflater.inflate(R.layout.fragment_classroom, container, false);
@@ -130,7 +128,7 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_managestaff, container, false);
         mAuth = FirebaseAuth.getInstance();
-        fstore=FirebaseFirestore.getInstance();
+        fstore = FirebaseFirestore.getInstance();
 
         dataInitialize();
 
@@ -140,6 +138,7 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
 
         return rootView;
     }
+
     public void onRefresh() {
         // Implement logic to refresh the list of staff members
         // Fetch the latest data from the Firestore database
@@ -167,7 +166,6 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
         recyclerview.setHasFixedSize(true);
 
 
-
         StaffAdapter staffAdapter = new StaffAdapter(getContext(), staffArrayList, new recyclerviewonclick() {
             @Override
             public void onItemClick(int position) {
@@ -186,8 +184,8 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
         teachername = new ArrayList<>();
         staffArrayList = new ArrayList<>();
         teacherId = new ArrayList<>(); // Initialize teacherId ArrayList
-        password=new ArrayList<>();
-        staffemail=new ArrayList<>();
+        password = new ArrayList<>();
+        staffemail = new ArrayList<>();
         // Get user names
         CollectionReference userCollectionRef = fstore.collection("user");
         task = userCollectionRef.get(); // Initialize the task here
@@ -198,8 +196,8 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         String userName = document.getString("fullname");
                         String userId = document.getId(); // Fetch the user ID
-                        String pass=document.getString("password");
-                        String email=document.getString("email");
+                        String pass = document.getString("password");
+                        String email = document.getString("email");
                         teachername.add(userName);
                         teacherId.add(userId); // Store the user ID
                         password.add(pass);
@@ -221,7 +219,7 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
 
         // Iterate through subject names and teacher names to create Subjects objects
         for (int j = 0; j < teachername.size(); j++) {
-            Staff staff_names = new Staff(teachername.get(j), teacherId.get(j),password.get(j), staffemail.get(j));
+            Staff staff_names = new Staff(teachername.get(j), teacherId.get(j), password.get(j), staffemail.get(j));
             staffArrayList.add(staff_names);
         }
 
@@ -253,80 +251,78 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
         textViewStaffInfo.setText("Teacher Name: " + staff.getTeacherName()); // Assuming you have a getter method for teacher name in Staff class
 
         // Set delete button click listener
-       buttonDelete.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               buttonDelete.setVisibility(View.GONE);
-               progressBar.setVisibility(View.VISIBLE);
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonDelete.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
 
 
-               DocumentReference userdoc= fstore.collection("user").document(staff.getTeacherID());
-               DocumentReference classteacher_doc = fstore.collection("classteachers").document(staff.getTeacherID());
-               userdoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                   @Override
+                DocumentReference userdoc = fstore.collection("user").document(staff.getTeacherID());
+                DocumentReference classteacher_doc = fstore.collection("classteachers").document(staff.getTeacherID());
 
-                   public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                       if(task.isSuccessful())
-                       {
-                           classteacher_doc.delete();
-                           userdoc.delete();
-                           mAuth.signOut();
-                           mAuth.signInWithEmailAndPassword(staff.getEmail(), staff.getpassword()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                               @Override
-                               public void onSuccess(AuthResult authResult) {
-                                   if(mAuth!=null)
-                                   {
-                                       mAuth.getCurrentUser().delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                           @Override
-                                           public void onSuccess(Void unused) {
-                                               staffmanagedialog.dismiss();
-                                               Toast.makeText(getContext(), "acc deleted", Toast.LENGTH_SHORT).show();
+                userdoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
 
-                                               mAuth.signInWithEmailAndPassword(getEmailFromSharedPreferences(),getPasswordFromSharedPreferences()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                                                   @Override
-                                                   public void onSuccess(AuthResult authResult) {
-                                                       Toast.makeText(getContext(), "previos acc login", Toast.LENGTH_SHORT).show();
-                                                       dataInitialize();
-                                                   }
-                                               }).addOnFailureListener(new OnFailureListener() {
-                                                   @Override
-                                                   public void onFailure(@NonNull Exception e) {
-                                                       Toast.makeText(getContext(), "failed to login admin", Toast.LENGTH_SHORT).show();
-                                                   }
-                                               });
-                                           }
-                                       }).addOnFailureListener(new OnFailureListener() {
-                                           @Override
-                                           public void onFailure(@NonNull Exception e) {
-                                               Toast.makeText(getContext(), "failed to delete acc", Toast.LENGTH_SHORT).show();
-                                           }
-                                       });
-                                   }
-                               }
-                           }).addOnFailureListener(new OnFailureListener() {
-                               @Override
-                               public void onFailure(@NonNull Exception e) {
-                                   Toast.makeText(getContext(), "sign in failed", Toast.LENGTH_SHORT).show();
-                                   mAuth.signInWithEmailAndPassword(getEmailFromSharedPreferences().toString(),getPasswordFromSharedPreferences().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                                       @Override
-                                       public void onSuccess(AuthResult authResult) {
-                                           Toast.makeText(getContext(), "previos acc login", Toast.LENGTH_SHORT).show();
-                                       }
-                                   }).addOnFailureListener(new OnFailureListener() {
-                                       @Override
-                                       public void onFailure(@NonNull Exception e) {
-                                           Toast.makeText(getContext(), "failed to login admin", Toast.LENGTH_SHORT).show();
-                                       }
-                                   });
-                               }
-                           });
-                       }
-                   }
-               });
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            classteacher_doc.delete();
+                            userdoc.delete();
+                            mAuth.signOut();
+                            mAuth.signInWithEmailAndPassword(staff.getEmail(), staff.getpassword()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    if (mAuth != null) {
+                                        mAuth.getCurrentUser().delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                staffmanagedialog.dismiss();
+                                                Toast.makeText(getContext(), "acc deleted", Toast.LENGTH_SHORT).show();
 
-           }
-       });
+                                                mAuth.signInWithEmailAndPassword(getEmailFromSharedPreferences(), getPasswordFromSharedPreferences()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                                    @Override
+                                                    public void onSuccess(AuthResult authResult) {
+                                                        Toast.makeText(getContext(), "previos acc login", Toast.LENGTH_SHORT).show();
+                                                        dataInitialize();
+                                                    }
+                                                }).addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(getContext(), "failed to login admin", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(getContext(), "failed to delete acc", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(), "sign in failed", Toast.LENGTH_SHORT).show();
+                                    mAuth.signInWithEmailAndPassword(getEmailFromSharedPreferences().toString(), getPasswordFromSharedPreferences().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                        @Override
+                                        public void onSuccess(AuthResult authResult) {
+                                            Toast.makeText(getContext(), "previos acc login", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(getContext(), "failed to login admin", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    }
+                });
 
+            }
+        });
 
 
         // Set close button click listener
@@ -334,7 +330,7 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(getContext(), staff.getEmail()+" : "+staff.getpassword(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), staff.getEmail() + " : " + staff.getpassword(), Toast.LENGTH_SHORT).show();
                 staffmanagedialog.dismiss(); // Dismiss the dialog
 
 
@@ -344,6 +340,9 @@ public class managestaffFragment extends Fragment implements SwipeRefreshLayout.
 
         staffmanagedialog.show();
     }
+
+    
+
     private String getEmailFromSharedPreferences() {
         SharedPreferences sharedPreferences = getContext().getSharedPreferences("CRED", Context.MODE_PRIVATE);
         return sharedPreferences.getString("email", "");
