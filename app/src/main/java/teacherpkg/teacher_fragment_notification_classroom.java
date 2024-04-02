dc7b4d1ec3246498d564b1dbdb7b46bfdbf73455
Varun Patil <varunpatil0217@gmail.com>
2024-04-03 01:04:29 +0530

    Prerna 14.0

--- app/src/main/java/teacherpkg/teacher_fragment_notification_classroom.java
+++ app/src/main/java/teacherpkg/teacher_fragment_notification_classroom.java
@@ -17,10 +17,5 @@ public class teacher_fragment_notification_classroom extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_teacher_fragment_notification_classroom);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
