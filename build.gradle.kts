b4b098c4cb202a3a0b2371d429996f66a6e35517
Suyog Raute <132730595+SuyogRaute@users.noreply.github.com>
2025-04-14 22:02:07 +0530

    update2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
+        classpath("com.google.gms:google-services:4.4.2")
 
     }
 }
@@ -14,7 +14,9 @@ buildscript {
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.3.0" apply false
+
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 dependencies {
 
